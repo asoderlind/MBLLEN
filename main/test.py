6249cd7ee0d77ae248721f8b382e53bd3c2e6f25
asoderlind <axesode@gmail.com>
2025-05-14 14:12:05 +0200

    update test

--- main/test.py
+++ main/test.py
@@ -1,6 +1,5 @@
 from glob import glob
 import numpy as np
-import scipy
 import keras
 import os
 import Network
@@ -8,16 +7,49 @@ import utls
 import time
 import cv2
 import argparse
+import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input", "-i", type=str, default='../input', help='test image folder')
-parser.add_argument("--result", "-r", type=str, default='../result', help='result folder')
-parser.add_argument("--model", "-m", type=str, default='Syn_img_lowlight_withnoise', help='model name')
-parser.add_argument("--com", "-c", type=int, default=1, help='output with/without origional image and mid result')
-parser.add_argument("--highpercent", "-hp", type=int, default=95, help='should be in [85,100], linear amplification')
-parser.add_argument("--lowpercent", "-lp", type=int, default=5, help='should be in [0,15], rescale the range [p%,1] to [0, 1]')
-parser.add_argument("--gamma", "-g", type=int, default=8, help='should be in [6,10], increase the saturability')
-parser.add_argument("--maxrange", "-mr", type=int, default=8, help='linear amplification range')
+parser.add_argument(
+    "--input", "-i", type=str, default="../input", help="test image folder"
+)
+parser.add_argument(
+    "--result", "-r", type=str, default="../result", help="result folder"
+)
+parser.add_argument(
+    "--model", "-m", type=str, default="Syn_img_lowlight_withnoise", help="model name"
+)
+parser.add_argument(
+    "--com",
+    "-c",
+    type=int,
+    default=1,
+    help="output with/without origional image and mid result",
+)
+parser.add_argument(
+    "--highpercent",
+    "-hp",
+    type=int,
+    default=95,
+    help="should be in [85,100], linear amplification",
+)
+parser.add_argument(
+    "--lowpercent",
+    "-lp",
+    type=int,
+    default=5,
+    help="should be in [0,15], rescale the range [p%,1] to [0, 1]",
+)
+parser.add_argument(
+    "--gamma",
+    "-g",
+    type=int,
+    default=8,
+    help="should be in [6,10], increase the saturability",
+)
+parser.add_argument(
+    "--maxrange", "-mr", type=int, default=8, help="linear amplification range"
+)
 arg = parser.parse_args()
 
 result_folder = arg.result
@@ -25,43 +57,55 @@ if not os.path.isdir(result_folder):
     os.makedirs(result_folder)
 
 input_folder = arg.input
-path = glob(input_folder+'/*.*')
+print("The input folder is " + input_folder)
+path = glob(input_folder + "/*")
+print("The number of test images is " + str(len(path)) + ".")
+
 
 model_name = arg.model
 mbllen = Network.build_mbllen((None, None, 3))
-mbllen.load_weights('../models/'+model_name+'.h5')
-opt = keras.optimizers.Adam(lr=2 * 1e-04, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-mbllen.compile(loss='mse', optimizer=opt)
+mbllen.load_weights("../models/" + model_name + ".h5")
+opt = keras.optimizers.Adam(
+    learning_rate=2 * 1e-04, beta_1=0.9, beta_2=0.999, epsilon=1e-08
+)
+mbllen.compile(loss="mse", optimizer=opt)
 
 flag = arg.com
 lowpercent = arg.lowpercent
 highpercent = arg.highpercent
-maxrange = arg.maxrange/10.
-hsvgamma = arg.gamma/10.
+maxrange = arg.maxrange / 10.0
+hsvgamma = arg.gamma / 10.0
+# tqdm_loader = tqdm.tqdm(total=len(path), desc="Processing", unit="image")
 for i in range(len(path)):
+    # tqdm_loader.update(1)
     img_A_path = path[i]
     img_A = utls.imread_color(img_A_path)
     img_A = img_A[np.newaxis, :]
 
-    starttime = time.clock()
+    print("The " + str(i + 1) + "th image's name is " + img_A_path)
+    starttime = time.perf_counter()
     out_pred = mbllen.predict(img_A)
-    endtime = time.clock()
-    print('The ' + str(i+1)+'th image\'s Time:' +str(endtime-starttime)+'s.')
+    endtime = time.perf_counter()
+    print("The " + str(i + 1) + "th image's Time:" + str(endtime - starttime) + "s.")
     fake_B = out_pred[0, :, :, :3]
     fake_B_o = fake_B
 
-    gray_fake_B = fake_B[:, :, 0] * 0.299 + fake_B[:, :, 1] * 0.587 + fake_B[:, :, 1] * 0.114
-    percent_max = sum(sum(gray_fake_B >= maxrange))/sum(sum(gray_fake_B <= 1.0))
+    gray_fake_B = (
+        fake_B[:, :, 0] * 0.299 + fake_B[:, :, 1] * 0.587 + fake_B[:, :, 1] * 0.114
+    )
+    percent_max = sum(sum(gray_fake_B >= maxrange)) / sum(sum(gray_fake_B <= 1.0))
     # print(percent_max)
     max_value = np.percentile(gray_fake_B[:], highpercent)
-    if percent_max < (100-highpercent)/100.:
+    if percent_max < (100 - highpercent) / 100.0:
         scale = maxrange / max_value
         fake_B = fake_B * scale
         fake_B = np.minimum(fake_B, 1.0)
 
-    gray_fake_B = fake_B[:,:,0]*0.299 + fake_B[:,:,1]*0.587 + fake_B[:,:,1]*0.114
+    gray_fake_B = (
+        fake_B[:, :, 0] * 0.299 + fake_B[:, :, 1] * 0.587 + fake_B[:, :, 1] * 0.114
+    )
     sub_value = np.percentile(gray_fake_B[:], lowpercent)
-    fake_B = (fake_B - sub_value)*(1./(1-sub_value))
+    fake_B = (fake_B - sub_value) * (1.0 / (1 - sub_value))
 
     imgHSV = cv2.cvtColor(fake_B, cv2.COLOR_RGB2HSV)
     H, S, V = cv2.split(imgHSV)
@@ -71,12 +115,12 @@ for i in range(len(path)):
     fake_B = np.minimum(fake_B, 1.0)
 
     if flag:
-        outputs = np.concatenate([img_A[0,:,:,:], fake_B_o, fake_B], axis=1)
+        outputs = np.concatenate([img_A[0, :, :, :], fake_B_o, fake_B], axis=1)
     else:
         outputs = fake_B
 
     filename = os.path.basename(path[i])
-    img_name = result_folder+'/' + filename
+    img_name = result_folder + "/" + filename
     # scipy.misc.toimage(outputs * 255, high=255, low=0, cmin=0, cmax=255).save(img_name)
     outputs = np.minimum(outputs, 1.0)
     outputs = np.maximum(outputs, 0.0)
